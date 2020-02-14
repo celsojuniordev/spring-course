8a3a749a33e087edb7f8c0ee0b0cd9ed85b49500
celsojuniordev <60205762+celsojuniordev@users.noreply.github.com>
2020-02-14 20:13:58 -0300

    Create request save and update DTO

---
+++ src/main/java/com/springcourse/dto/RequestSaveDTO.java
@@ -0,0 +1,35 @@
+package com.springcourse.dto;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestSaveDTO {
+
+    @NotBlank(message = "Assunto é obrigatório")
+    private String subject;
+
+    private String description;
+
+    @NotNull(message = "Proprietario é obrigatório")
+    private User owner;
+    private List<RequestStage> stages = new ArrayList<>();
+
+    public Request transformToRequest() {
+        Request request = new Request(null, this.subject, this.description, null, null, this.owner, stages);
+        return request;
+    }
+}
---
+++ src/main/java/com/springcourse/dto/RequestUpdateDTO.java
@@ -0,0 +1,39 @@
+package com.springcourse.dto;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import com.springcourse.enums.RequestState;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestUpdateDTO {
+
+    @NotBlank(message = "Assunto é obrigatório")
+    private String subject;
+
+    private String description;
+
+    @NotNull(message = "Estado é obeigatório")
+    private RequestState state;
+
+    @NotNull(message = "Proprietario é obrigatório")
+    private User owner;
+    private List<RequestStage> stages = new ArrayList<>();
+
+    public Request transformToRequest() {
+        Request request = new Request(null, this.subject, this.description, null, this.state, this.owner, stages);
+        return request;
+    }
+}
