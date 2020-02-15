79344cc4a6592495f4a9d19e248ad72f808d8953
celsojuniordev <60205762+celsojuniordev@users.noreply.github.com>
2020-02-15 14:22:15 -0300

    Config to retrieve errors for invalid fields

--- src/main/java/com/springcourse/resource/ResourceExceptionHandler.java
+++ src/main/java/com/springcourse/resource/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.springcourse.resource;
 
 import com.springcourse.esception.NotFoundException;
 import com.springcourse.resource.exception.ApiError;
+import com.springcourse.resource.exception.ApiErrorList;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @ControllerAdvice
 public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
@@ -25,8 +28,13 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String defaultMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date());
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach( error -> {
+            errors.add(error.getDefaultMessage());
+        });
+
+        String defaultMessage = "Campo(s) inválido(s)";
+        ApiErrorList error = new ApiErrorList(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date(), errors);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
