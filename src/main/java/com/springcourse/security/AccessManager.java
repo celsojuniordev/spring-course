7ae948abe69075aebdfa40ea7123a454b2c8c675
celsojuniordev <60205762+celsojuniordev@users.noreply.github.com>
2020-03-03 23:03:05 -0300

    Create access manager

---
+++ src/main/java/com/springcourse/security/AccessManager.java
@@ -0,0 +1,46 @@
+package com.springcourse.security;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.User;
+import com.springcourse.exception.NotFoundException;
+import com.springcourse.repository.UserRepository;
+import com.springcourse.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("accessManager")
+public class AccessManager {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RequestService requestService;
+
+    public boolean isOwner(Long id) {
+        String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (user.isPresent()) throw new NotFoundException("Usuário não encontrado com o email ->" + email);
+
+        User result = user.get();
+
+        return result.getId() == id;
+    }
+
+    public boolean isRequestOwner(Long id) {
+        String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (user.isPresent()) throw new NotFoundException("Usuário não encontrado com o email ->" + email);
+
+        User result = user.get();
+
+        Request request = requestService.findById(id);
+
+        return result.getId() == request.getOwner().getId();
+    }
+}
