0fb613e35e2c07483010a19c181eb9e6cc38dd13
celsojuniordev <60205762+celsojuniordev@users.noreply.github.com>
2020-03-21 10:31:05 -0300

    Config sorting

--- src/main/java/com/springcourse/model/PageRequestModel.java
+++ src/main/java/com/springcourse/model/PageRequestModel.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Getter
@@ -16,14 +19,33 @@ public class PageRequestModel {
 
     private int page = 0;
     private int size = 10;
+    private String sort = "";
 
     public PageRequestModel (Map<String, String> params) {
         if (params.containsKey("page")) page = Integer.parseInt(params.get("page"));
         if (params.containsKey("size")) size = Integer.parseInt(params.get("size"));
+        if (params.containsKey("sort")) sort = params.get("sort");
 
     }
 
     public PageRequest toSpringPageRequest() {
-        return PageRequest.of(page, size);
+        List<Sort.Order> orders = new ArrayList<>();
+        String[] properties = sort.split(",");
+
+        for(String prop : properties) {
+            if (prop.trim().length() > 0) {
+                String column = prop.trim();
+
+                if (column.startsWith("-")) {
+                    column = column.replace("-", "").trim();
+
+                    orders.add(Sort.Order.desc(column));
+                } else {
+                    orders.add(Sort.Order.asc(column));
+                }
+            }
+        }
+
+        return PageRequest.of(page, size, Sort.by(orders));
     }
 }
